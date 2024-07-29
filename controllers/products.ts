02124cb2f5e6fa5f18fb39f9efb31802b1d28ebd
Alonso Cortez <practicas.0061@sesitdigital.com>
2024-07-28 20:42:44 -0500

    deploy

--- controllers/products.ts
+++ controllers/products.ts
@@ -64,7 +64,7 @@ export const editProductDetail= async (req: Request, res: Response) => {
 
 export const deleteProduct= async (req: Request, res: Response) => {
     try {
-        const { id }= req.body;
+        const { id }= req.params;
         const product= await Product.findOne({
             where: {
                 id: id
